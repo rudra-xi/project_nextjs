7692ed376eb656d61d6e4b462850ffc657813cf6
Rudra <goutam.prasad.2707@gmail.com>
2025-07-05 21:33:15 +0530

    metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,11 @@
+export const metadata = {
+	title: "About Us Metadata",
+};
+
 export default function About() {
-     return (
-          <div className="flex min-h-screen items-center justify-center bg-black text-white">
-               <h1 className="text-4xl font-bold">About Us</h1>
-          </div>
-     );
+	return (
+		<div className="flex min-h-screen items-center justify-center bg-black text-white">
+			<h1 className="text-4xl font-bold">About Us</h1>
+		</div>
+	);
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,23 @@
+import { Metadata } from "next";
+
+type Props = {
+	params: Promise<{
+		productId: string;
+	}>;
+};
+
+export const generateMetadata = async ({
+	params,
+}: Props): Promise<Metadata> => {
+	const id = (await params).productId;
+	return {
+		title: `Product ${id} Metadata`,
+		icons: {
+			icon: "🙌",
+		},
+	};
+};
+
 export default async function ProductDetails({
 	params,
 }: {
