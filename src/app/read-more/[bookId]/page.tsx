a5ac13764f05b5ea43bde969fdb0b972e4f0e704
Rudra <goutam.prasad.2707@gmail.com>
2025-07-05 22:26:02 +0530

    params & searchParams

--- src/app/read-more/[bookId]/page.tsx
+++ src/app/read-more/[bookId]/page.tsx
@@ -1,37 +1,36 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function BookId({
+	params,
+	searchParams,
+}: {
+	params: { bookId: string };
+	searchParams: { lang?: "en" | "fr" | "es" };
+}) {
+	const { bookId } = params;
+	const { lang = "en" } = searchParams;
 	return (
 		<div className="flex flex-col min-h-screen items-center justify-center bg-black text-white">
-			<h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-			<Link
-				href={"/about"}
-				className="text-blue-500 hover:underline mt-6"
-			>
-				About
-			</Link>
-			<Link
-				href={"/products"}
-				className="text-blue-500 hover:underline mt-2"
-			>
-				Products
-			</Link>
+			<h1 className="text-4xl font-bold">Book Details</h1>
+
+			<p className="text-lg mt-4">Book Id {bookId}</p>
+			<p className="text-lg mt-4">Reading Book in {lang}</p>
 
 			<div className="mt-8 flex flex-col items-center">
 				<Link
-					href={"read-more/bookId?lang=en"}
+					href={`/read-more/${bookId}?lang=en`}
 					className="text-blue-500 hover:underline mt-2"
 				>
 					Read more in English
 				</Link>
 				<Link
-					href={"read-more/bookId?lang=fr"}
+					href={`/read-more/${bookId}?lang=fr`}
 					className="text-blue-500 hover:underline mt-2"
 				>
 					Read more in French
 				</Link>
 				<Link
-					href={"read-more/bookId?lang=es"}
+					href={`/read-more/${bookId}?lang=es`}
 					className="text-blue-500 hover:underline mt-2"
 				>
 					Read more in Spanish
