a0f93a86eb2f20628dfa9026ef4098afd29787c0
Rudra <goutam.prasad.2707@gmail.com>
2025-07-08 13:01:41 +0530

    Loading

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,7 @@
+export default function BlogLoading() {
+	return (
+		<div className="flex min-h-screen text-6xl items-center justify-center bg-black text-white">
+			Loading...
+		</div>
+	);
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,5 @@
-export default function Blog() {
+export default async function Blog() {
+	await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay
 	return (
 		<div className="flex min-h-screen items-center justify-center bg-black text-white">
 			<h1 className="text-4xl font-bold">Blog Page</h1>
