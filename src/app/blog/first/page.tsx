aadd119b186c9224e6274d2fa9b385db48b69a19
Rudra <goutam.prasad.2707@gmail.com>
2025-07-15 20:55:54 +0530

    redirect in Route Handlers

--- src/app/blog/first/page.tsx
+++ src/app/blog/first/page.tsx
@@ -1,4 +1,7 @@
+import { redirect } from "next/navigation";
+
 export default function FirstBlogPost() {
+	redirect("/blog/second");
 	return (
 		<div>
 			<h1 className="flex min-h-screen items-center justify-center bg-black text-white text-4xl font-bold">
