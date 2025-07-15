b27d88ede870786be3b3991b26f8e9d16567561c
Rudra <goutam.prasad.2707@gmail.com>
2025-07-15 20:44:12 +0530

    cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 export async function GET(request: NextRequest) {
 	// const reqHeaders = new Headers(request.headers);
@@ -8,9 +8,17 @@ export async function GET(request: NextRequest) {
 	const headersList = await headers();
 	console.log(headersList.get("Authorization"));
 	// return Response.json({ message: "Profile API is working" });
+
+	const theme = request.cookies.get("theme");
+	console.log(theme);
+
+	const cookiesStore = await cookies();
+	cookiesStore.set("resultPerPage", "20");
+
 	return new Response("<h1>Profile API is working</h1>", {
 		headers: {
 			"Content-Type": "text/html",
+			"Set-Cookie": "theme=dark",
 		},
 	});
 }
